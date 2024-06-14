4ff259377c11aad983cfe164709d4f7480576c69
Rodrigo de Alvarenga Mattos <rodrigo@bragile.com>
2024-06-13 22:16:47 -0300

    removed docker-registry and traefik

--- manifests/platform/secret/docker-secret.sh
+++
@@ -1,6 +0,0 @@
-kubectl create secret docker-registry docker-registry-secret \
-  --docker-server=docker-registry.registry.svc.cluster.local:5000 \
-  --docker-username=dappmesh \
-  --docker-password=dappmesh \
-  --namespace=dappmesh \
-  --dry-run=client -o yaml > ./manifests/platform/secret/docker-registry.yaml
