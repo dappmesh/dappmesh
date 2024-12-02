fb9e9ed0044ff18872962c555f4fb59ed8ec306e
Rodrigo de Alvarenga Mattos <66392427+rodrigoalvamat@users.noreply.github.com>
2024-12-01 21:10:12 -0300

    Update reconcile.rs

--- crates/domain-operator/tests/reconcile.rs
+++ crates/domain-operator/tests/reconcile.rs
@@ -22,16 +22,17 @@ mod tests {
 		let domain_api: Api<DappDomain> = Api::namespaced(client.clone(), DOMAIN_NAMESPACE);
 		let domain_resource = DappDomain::new(DOMAIN_NAME, DappDomainSpec::default());
 
-		let controller = DomainOperatorController::new(
+		let operator_client = client.clone();
+		let operator = DomainOperatorController::new(
 			DOMAIN_NAME.to_string(),
 			DOMAIN_NAMESPACE.to_string(),
-			&client,
+			&operator_client,
 		);
 
 		let context: Arc<OperatorContext> = Arc::new(OperatorContext::new(client.clone()));
 		let domain = domain_api.create(&PostParams::default(), &domain_resource).await?;
 
-		let action = controller.reconcile(Arc::new(domain), context.clone()).await?;
+		let action = operator.reconcile(Arc::new(domain), context.clone()).await?;
 		ensure!(action == Action::requeue(Duration::from_secs(5)));
 
 		let finalizers = domain_api.get(DOMAIN_NAME).await?.metadata.finalizers;
@@ -39,7 +40,7 @@ mod tests {
 
 		let domain = domain_api.delete(DOMAIN_NAME, &DeleteParams::default()).await?;
 
-		let action = controller.reconcile(Arc::new(domain.left_or_default()), context).await?;
+		let action = operator.reconcile(Arc::new(domain.left_or_default()), context).await?;
 		ensure!(action == Action::await_change());
 
 		Ok(())
